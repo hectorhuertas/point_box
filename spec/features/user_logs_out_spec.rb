bf6e4a1aaafad8d033237ee4e88e0195a5edf4da
Dan Winter <dan.j.winter@gmail.com>
2016-01-09 13:16:57 -0700

    add stub for current_user

--- spec/features/user_logs_out_spec.rb
+++ spec/features/user_logs_out_spec.rb
@@ -3,16 +3,13 @@ require 'rails_helper'
 RSpec.describe "user logs out" do
   scenario "they see the login page" do
     user = FactoryGirl.create(:user, password: "pass")
+    ApplicationController.any_instance.stub(:current_user).and_return(user)
 
-    visit login_path
-
-    fill_in "Username", with: "Peter"
-    fill_in "Password", with: "pass"
-    click_on "Login"
-
+    visit user_path(user)
     click_on "Logout"
 
     expect(page).to have_content "Goodbye, Peter"
     expect(current_path).to eq login_path
+
   end
 end
